f61846f2f3bfe2d154600e82ae0b7dedd25f8b46
judeharding <jharding@genesisgroupusa.com>
2020-08-13 16:11:22 -0400

    working up to 190

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,25 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+//      theme: ThemeData.dark().copyWith(
+//        textTheme: TextTheme(
+//          bodyText2: TextStyle(color: Colors.black54),
+//        ),
+//      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        // new way with id's doesn't make an entire welcome screen regenerate
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+//        // old way
+//        '/': (context) => WelcomeScreen(),
+//        '/login': (context) => LoginScreen(),
+//        '/registration': (context) => RegistrationScreen(),
+//        '/chat': (context) => ChatScreen(),
+      },
+//      home: WelcomeScreen(),
     );
   }
 }
